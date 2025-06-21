73dcaaf161f58feb5af6a10401a956aacbc918fd
LewdLillyVT <lewdlillycontact@gmail.com>
2025-06-21 07:10:06 +0200

    Add files via upload

--- modules/output.go
+++ modules/output.go
@@ -5,12 +5,36 @@ import (
 	"path/filepath"
 )
 
+// customOutputDir stores the user-selected output directory. If empty, the default path is used.
+var customOutputDir string
+
+// SetCustomOutputDir sets the custom output directory. This function should be called
+// when the user selects a new directory via the settings.
+func SetCustomOutputDir(path string) {
+	customOutputDir = path
+}
+
+// GetCustomOutputDir returns the currently set custom output directory.
+// This can be useful for displaying the selected path in the UI.
+func GetCustomOutputDir() string {
+	return customOutputDir
+}
+
 // EnsureOutputDir creates the output directory and returns its path.
+// It uses the customOutputDir if it has been set; otherwise, it defaults
+// to `%LOCALAPPDATA%\Temp\DiagnosticsFiles`.
 func EnsureOutputDir() (string, error) {
-	outputDir := filepath.Join(os.Getenv("LOCALAPPDATA"), "Temp", "DiagnosticsFiles")
-	err := os.MkdirAll(outputDir, os.ModePerm)
+	var targetDir string
+	if customOutputDir != "" {
+		targetDir = customOutputDir
+	} else {
+		targetDir = filepath.Join(os.Getenv("LOCALAPPDATA"), "Temp", "DiagnosticsFiles")
+	}
+
+	// Create all necessary parent directories if they don't exist
+	err := os.MkdirAll(targetDir, os.ModePerm)
 	if err != nil {
 		return "", err
 	}
-	return outputDir, nil
+	return targetDir, nil
 }
